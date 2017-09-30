27753fd16f564383a8407209c733797db3d3d989
BRIAN GERHARDS <BRIANGERHARDS@GMAIL.COM>
2017-09-30 18:48:13 -0500

    Added finishing touches to the script.

--- win-script-disable-touchpad.bat
+++ win-script-disable-touchpad.bat
@@ -1,12 +1,39 @@
 @echo off
 
-echo Hello. This script will create a new registry value disabling the touchpad when a mouse is connected.
-echo This can be reversed by deleting the following registry key value:
-echo HKEY_LOCAL_MACHINE\SOFTWARE\Synaptics\SynTPEnh\DisableIntPDFeature
-echo.
+setlocal ENABLEEXTENSIONS
+set KEY_NAME="HKLM\SOFTWARE\Synaptics\SynTPEnh"
+set VALUE_NAME="DisableIntPDFeature"
+set VALUE_DATA=0x33
+set VALUE_TYPE=REG_DWORD
 
-reg add "HKLM\SOFTWARE\Synaptics\SynTPEnh"  /f /v "DisableIntPDFeature" /t REG_DWORD /d "51"
-echo.
-reg query "HKLM\SOFTWARE\Synaptics\SynTPEnh" /v "DisableIntPDFeature"
+@echo Adding registry value %KEY_NAME% \ %VALUE_NAME%
+reg add %KEY_NAME%  /f /v %VALUE_NAME% /t %VALUE_TYPE% /d %VALUE_DATA%
 
+reg query %KEY_NAME% /v %VALUE_NAME%
+FOR /F "usebackq skip=2 tokens=1-3" %%A IN (`REG QUERY %KEY_NAME% /v %VALUE_NAME% 2^>nul`) DO (
+    set ValueName=%%A
+    set ValueType=%%B
+    set ValueValue=%%C
+)
+
+if defined ValueName (
+    @echo Value Name = %ValueName%
+    @echo Value Type = %ValueType%
+    @echo Value Value = %ValueValue%
+    @echo ----------------------------
+    if "%ValueValue%" == "%VALUE_DATA%" (
+        @echo Registry value set correctly
+    ) else (
+        @echo Registry value not correctly
+        @echo  ----------------------------
+        @echo Please attempt to set this value manually. Open your registry
+        @echo  editor and modify the following:
+        @echo        Key: %KEY_NAME%
+        @echo        Value Name: %VALUE_NAME%
+        @echo        Value Data: %VALUE_DATA%
+        @echo        Value Type: DWORD %VALUE_TYPE%
+    )
+) else (
+    @echo %KEY_NAME%\%VALUE_NAME% not found.
+)
 PAUSE
